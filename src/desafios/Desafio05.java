c6e850414ee69b16dbed57a61d430c6615a78312
Geovane_Andrade <geovanedeandradegrangeiro@gmail.com>
2023-09-13 18:50:39 -0300

    Desafios 05 e 06

---
+++ src/desafios/Desafio05.java
@@ -0,0 +1,17 @@
+package desafios;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.OptionalDouble;
+
+public class Desafio05 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+        OptionalDouble average = numeros.stream()
+                .filter(n -> n > 5)
+                .mapToInt(n -> n)
+                .average();
+        System.out.println(average);
+
+    }
+}
