6b1082a3d2e440f27a310c9fe6462e92a0269194
Geovane_Andrade <geovanedeandradegrangeiro@gmail.com>
2023-09-21 18:25:52 -0300

    Desafios 10,11 e 12

---
+++ src/desafios/Desafio11.java
@@ -0,0 +1,21 @@
+package desafios;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class Desafio11 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+        List<Integer> collect = numeros.stream()
+                .map(n -> n * n)
+                .reduce(Integer::sum)
+                .stream().collect(Collectors.toList());
+
+        System.out.println(collect);
+
+
+    }
+}
